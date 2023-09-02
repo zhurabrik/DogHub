a9cea06cdd6fb28d33554a9002b2581a90d1fef9
daria <feltro@horek.lan>
2023-09-02 18:52:34 +0300

    change initial templates

--- backend/db/migrations/001_initial.sql
+++ backend/db/migrations/001_initial.sql
@@ -3,7 +3,17 @@ CREATE TABLE IF NOT EXISTS templates (
   meta JSONB NOT NULL
 );
 
-INSERT INTO templates (id, meta) values
-(1, '{"path": "some/path/to/image"}'),
-(2, '{"path2": "some/path/to/image2"}')
+INSERT INTO templates (meta) values
+('{
+  "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+  "description": "just a picture",
+  "title": "just a picture",
+  "address": "some address"
+}'),
+('{
+  "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Bratskfountain.jpg/300px-Bratskfountain.jpg",
+  "description": "just a picture of Bratsk",
+  "title": "just a picture of Bratsk",
+  "address": "some address"
+}')
 ON CONFLICT (id) DO NOTHING;
